1e761b83af0663f7b99784825c5a389026992a6b
Jake Burton <jburto94@yahoo.com>
2022-09-19 12:41:56 -0400

    added typing text component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { Icon } from '@iconify/react';
+import TypingText from './TypingText';
 
 const Header = () => {
   return (
@@ -6,6 +7,7 @@ const Header = () => {
       <div className='container d-flex align-items-center justify-content-center flex-column'>
         <Icon icon="fa-solid:laptop-code" color="white" width="150" />
         <h1 className='text-center text-white my-4'>Jake Burton</h1>
+        <TypingText />
       </div>
     </header>
   );
--- src/components/TypingText.js
+++ src/components/TypingText.js
@@ -1,8 +1,68 @@
-const { useState, useRef, useEffect } = 'react';
+import { useState, useRef, useEffect } from 'react';
 
-const TypingText = words => {
+const words = ['Web Developer', 'Fullstack engineer', 'UI Developer'];
+const firstWord = words[0].split('')
+
+const TypingText = () => {
+  const [wordIndex, setWordIndex] = useState(0);
+  const [currentWord, setCurrentWord] = useState(firstWord);
+  const direction = useRef('backward');
+  const letterIndex = useRef();
+  const typingInterval = useRef();
+
+  useEffect(() => {
+    let pauseCounter = 0;
+
+    const typeLetter = () => {
+      if (letterIndex.current >= words[wordIndex].length) {
+        direction.current = 'backward';
+
+        pauseCounter = 6;
+        return;
+      }
+
+      const segment = words[wordIndex].split('');
+      setCurrentWord(currentWord.concat(segment[letterIndex.current]));
+      letterIndex.current++;
+    }
+
+    const backspace = () => {
+      if (letterIndex.current === 0) {
+        const isLastWord = wordIndex === words.length - 1;
+
+        setWordIndex(isLastWord ? 0 : wordIndex + 1);
+        direction.current = 'forward';
+        return;
+      }
+
+      const segment = currentWord.slice(0, currentWord.length - 1);
+      setCurrentWord(segment);
+      letterIndex.current = currentWord.length - 1;
+    }
+
+    typingInterval.current = setInterval(() => {
+      if (pauseCounter > 0) {
+        pauseCounter--;
+        return;
+      }
+
+      if (direction.current === 'forward') {
+        typeLetter();
+      } else {
+        backspace();
+      }
+    }, 100);
+
+    return () => {
+      clearInterval(typingInterval.current);
+    }
+
+  }, [wordIndex, currentWord, direction]);
+
   return (
-
+    <h2 className={`word ${currentWord.length ? 'full' : 'empty'} text-uppercase`}>
+      <span>{currentWord.length ? currentWord.join('') : '0'}</span>
+    </h2>
   );
 };
 
